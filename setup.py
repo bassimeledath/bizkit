c32dc34e76a0f24b73b89565105e177690b2adc4
Bassim <40384860+bassimeledath@users.noreply.github.com>
2019-07-21 12:09:53 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bizkit",
-    version="0.0.1",
+    version="0.0.2",
     author="Bassim Eledath, Lynn He, Christine Zhu, Amanda Ma",
     author_email="bassimfaizal@gmail.com",
     description="A small example package",
