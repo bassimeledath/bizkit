db1c9f70a5ab77bcf3425cd3d89facc47d05e82a
Bassim <40384860+bassimeledath@users.noreply.github.com>
2019-07-22 10:19:03 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[,
+    install_requires=[
         "pandas",
         "lifelines",
         "bokeh"
