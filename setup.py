2c46b26682ea8acb06898d42ab3f74b1f81ca32e
bassim.eledath <bassim.eledath@duke.edu>
2019-07-30 14:19:45 -0400

    change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bizkit",
-    version="0.0.5",
+    version="0.0.1",
     author="Bassim Eledath, Lynn He, Christine Zhu, Amanda Ma",
     author_email="bassimfaizal@gmail.com",
     description="A package that streamlines business data analytics",
